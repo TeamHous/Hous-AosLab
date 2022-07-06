7da6b8bfe77a42e68dcd961e0f491ddc8550f1cb
이영주 <yungju9982@naver.com>
2022-07-07 00:30:30 +0900

    #3 [feat] ActivityHome 데이터바인딩

--- app/src/main/java/com/hous/housaoslab/HomeActivity.kt
+++ app/src/main/java/com/hous/housaoslab/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.hous.housaoslab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hous.housaoslab.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        setContentView(binding.root)
     }
 }
