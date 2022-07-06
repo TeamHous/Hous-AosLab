81a7fc267d3e2d95d01061f5468aee089110dd6e
이영주 <yungju9982@naver.com>
2022-07-07 02:23:25 +0900

    #3 [feat] 바텀내비게이터 색상 변경 기능 추가

--- app/src/main/java/com/hous/housaoslab/HomeActivity.kt
+++ app/src/main/java/com/hous/housaoslab/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.hous.housaoslab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.hous.housaoslab.databinding.ActivityHomeBinding
 
@@ -12,7 +13,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         setContentView(binding.root)
-        binding.botNavHome.itemIconTintList = null
         binding.botNavHome.selectedItemId = R.id.ic_bot_nav_home
+
+        binding.botNavHome.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_bot_nav_home -> {
+                    binding.botNavHome.background = ContextCompat.getDrawable(this, R.drawable.shape_yellow_fill_30_rectangle)
+                    binding.botNavHome.itemTextColor = ContextCompat.getColorStateList(this, R.color.sel_bot_nav_home_color)
+                    binding.botNavHome.itemIconTintList = ContextCompat.getColorStateList(this, R.color.sel_bot_nav_home_color)
+                    true
+                }
+                R.id.ic_bot_nav_rules -> {
+                    binding.botNavHome.background = ContextCompat.getDrawable(this, R.drawable.shape_blue_fill_30_rectangle)
+                    binding.botNavHome.itemTextColor = ContextCompat.getColorStateList(this, R.color.sel_bot_nav_rules_color)
+                    binding.botNavHome.itemIconTintList = ContextCompat.getColorStateList(this, R.color.sel_bot_nav_rules_color)
+                    true
+                }
+                else -> {
+                    binding.botNavHome.background = ContextCompat.getDrawable(this, R.drawable.shape_red_fill_30_rectangle)
+                    binding.botNavHome.itemTextColor = ContextCompat.getColorStateList(this, R.color.sel_bot_nav_profile_color)
+                    binding.botNavHome.itemIconTintList = ContextCompat.getColorStateList(this, R.color.sel_bot_nav_profile_color)
+                    true
+                }
+            }
+        }
     }
 }
