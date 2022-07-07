eec6accd611cb7319dc724ea19a4c62fa7c2bb74
이영주 <yungju9982@naver.com>
2022-07-08 04:19:27 +0900

    #12 [feat] ComingUpAdapter 생성

---
+++ app/src/main/java/com/hous/housaoslab/ui/home/model/ComingUpAdapter.kt
@@ -0,0 +1,37 @@
+package com.hous.housaoslab.ui.home.model
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hous.housaoslab.databinding.ItemHomeComingUpDetailBinding
+
+class ComingUpAdapter : RecyclerView.Adapter<ComingUpAdapter.ComingUpViewHolder>() {
+    val comingUpList = mutableListOf<ComingUpData>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComingUpViewHolder {
+        val binding =
+            ItemHomeComingUpDetailBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return ComingUpViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ComingUpViewHolder, position: Int) {
+        holder.onBind(comingUpList[position])
+    }
+
+    override fun getItemCount(): Int = comingUpList.size
+
+    class ComingUpViewHolder(
+        private val binding: ItemHomeComingUpDetailBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ComingUpData) {
+            binding.ivHomeComingUpShape.setImageResource(data.shape)
+            binding.ivHomeComingUpImage.setImageResource(data.image)
+            binding.tvHomeComingUpDay.text = data.day
+            binding.position = adapterPosition
+        }
+    }
+}
