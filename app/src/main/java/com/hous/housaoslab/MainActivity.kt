ff4924e02467765c65a2c50f56bbf8a5a77ba665
murjune <sis9221302@gmail.com>
2022-07-07 17:50:52 +0900

    #9 [FEAT] MainActivity에서 버튼 클릭시 EventDialog 띄우기 기능 추가

--- app/src/main/java/com/hous/housaoslab/MainActivity.kt
+++ app/src/main/java/com/hous/housaoslab/MainActivity.kt
@@ -2,10 +2,41 @@ package com.hous.housaoslab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.hous.housaoslab.databinding.ActivityMainBinding
+import com.hous.housaoslab.util.showToast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EventDialogFragment.MyCustomFragmentListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        onClickDialogButton()
+        setContentView(binding.root)
+    }
+
+    private fun onClickDialogButton() {
+        binding.btnDialog.setOnClickListener {
+            val dialog = EventDialogFragment()
+            dialog.show(supportFragmentManager, EVENT_DIALOG_TAG)
+        }
+    }
+
+    override fun onClickCloseIcon(dialog: DialogFragment) {
+        dialog.dismiss()
+    }
+
+    override fun onClickSaveButton(dialog: DialogFragment) {
+        showToast("저장버튼을 눌렀습니다.")
+    }
+
+    override fun onClickDeleteButton(dialog: DialogFragment) {
+        showToast("삭제 버튼을 눌렀습니다.")
+    }
+
+    companion object {
+        private const val TAG = "로그"
+        private const val EVENT_DIALOG_TAG = "MyCustomFragment"
     }
 }
