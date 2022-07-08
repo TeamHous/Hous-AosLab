0ff2a930b33d53e6c1133b48534dffb7118300fd
이영주 <yungju9982@naver.com>
2022-07-08 17:55:35 +0900

    #15 [feat] HomeFragment에 RulesAdpater 연결

--- app/src/main/java/com/hous/housaoslab/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hous/housaoslab/ui/home/HomeFragment.kt
@@ -1,18 +1,22 @@
-package com.hous.housaoslab
+package com.hous.housaoslab.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hous.housaoslab.R
 import com.hous.housaoslab.databinding.FragmentHomeBinding
 import com.hous.housaoslab.ui.home.model.ComingUpAdapter
 import com.hous.housaoslab.ui.home.model.ComingUpData
+import com.hous.housaoslab.ui.home.model.RulesAdapter
+import com.hous.housaoslab.ui.home.model.RulesData
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     val binding get() = _binding!!
     private lateinit var comingUpAdapter: ComingUpAdapter
+    private lateinit var rulesAdapter: RulesAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -40,6 +44,20 @@ class HomeFragment : Fragment() {
                 ComingUpData(R.drawable.shape_yellow_fill_16_rectangle, R.drawable.img_coming_up_party, "D-20"),
                 ComingUpData(R.drawable.shape_yellow_fill_16_rectangle, R.drawable.img_coming_up_party, "D-25"),
                 ComingUpData(R.drawable.shape_yellow_fill_16_rectangle, R.drawable.img_coming_up_party, "D-80"),
+            )
+        )
+        comingUpAdapter.notifyDataSetChanged()
+
+        rulesAdapter = RulesAdapter()
+        binding.rvHomeRules.adapter = rulesAdapter
+
+        rulesAdapter.rulesList.addAll(
+            listOf(
+                RulesData("00시~ 불 끄기!"),
+                RulesData("23시~ 이어폰 필수!"),
+                RulesData("세탁기는 화,수,토"),
+                RulesData("일 - 청소하는 날!"),
+                RulesData("2,4주 토- 장보기"),
             )
         )
         comingUpAdapter.notifyDataSetChanged()
