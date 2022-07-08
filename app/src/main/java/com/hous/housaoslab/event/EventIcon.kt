b2975e5bc0e2a78e3b5dc5ac2372ded1f4e16c7b
murjune <sis9221302@gmail.com>
2022-07-08 21:09:05 +0900

    #9 [feat] EventIcon enum class 구현

---
+++ app/src/main/java/com/hous/housaoslab/event/EventIcon.kt
@@ -0,0 +1,8 @@
+package com.hous.housaoslab.event
+
+import com.hous.housaoslab.R
+
+enum class EventIcon(val index: Int, val drawableRes: Int) {
+    FIRST(0, R.drawable.ic_dialog_party), SECOND(1, R.drawable.ic_3d_example),
+    THIRD(2, R.drawable.ic_dialog_close), FOURTH(3, R.drawable.ic_launcher_background);
+}
