9a4ec25da1ffe59a2b2735fe5f87dd818838af2e
이영주 <yungju9982@naver.com>
2022-07-08 17:53:39 +0900

    #15 [chore] HomeFragment 위치 변경

--- app/src/main/java/com/hous/housaoslab/ui/home/MainActivity.kt
+++ app/src/main/java/com/hous/housaoslab/ui/home/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.hous.housaoslab.HomeFragment
 import com.hous.housaoslab.R
 import com.hous.housaoslab.databinding.ActivityMainBinding
 
